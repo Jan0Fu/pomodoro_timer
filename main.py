3e83c267f41bcbab1b027b0153976b96004fadee
Jan0Fu <115988224+Jan0Fu@users.noreply.github.com>
2022-12-08 12:14:57 +0100

    Finished main countdown logic

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from tkinter import *
+import tkmacosx
+import math
+
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -12,21 +15,36 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    count_down(5 * 60)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro Timer")
-window.config(padx=100, pady=50, bg=YELLOW, highlightthickness=0)
+window.config(padx=100, pady=50, bg=YELLOW)
 
-canvas = Canvas(width=200, height=224, bg=YELLOW)
+timer_label = Label(text="Timer", fg=GREEN, font=(FONT_NAME, 50), bg=YELLOW)
+timer_label.grid(column=1, row=0)
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
-canvas.create_image(103, 112, image=tomato_img)
-canvas.create_text(103, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.create_image(100, 112, image=tomato_img)
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
 
-
-
+button_start = tkmacosx.Button(text="Start", borderless=1, command=start_timer)
+button_start.grid(column=0, row=2)
+button_reset = tkmacosx.Button(text="Reset", borderless=1)
+button_reset.grid(column=2, row=2)
+checkmark = Label(text="✔", fg=GREEN, bg=YELLOW)
+checkmark.grid(column=1, row=3)
 
 window.mainloop()
